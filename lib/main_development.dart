0be9bbd617ad544b073027267e4da87a9765fc9e
IBTESHAM-AHMED <117981993+IBTESHAM-AHMED@users.noreply.github.com>
2025-07-07 02:17:41 +0800

    applied firebase intializer in both main prod & dev files

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:utmmart/core/depandancy_injection/service_locator.dart';
 import 'package:utmmart/core/utils/service_locator/service_locator.dart';
 import 'package:utmmart/t_store.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:utmmart/firebase_options.dart'; // Add this import
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await setupServiceLocator();
 
-  await setupOldServiceLocator();
+  // Initialize Firebase first
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Then setup dependency injection
+  await setupServiceLocator();
+  setupOldServiceLocator(); // Note: No await here since it's synchronous
 
   runApp(const TStore());
 }
